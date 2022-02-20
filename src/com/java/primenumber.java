e8238a531df3ba4986ea29d87ac0e2c2c7357cfa
Ranidhumma <ranidhumma8@gmail.com>
2022-02-20 21:26:20 +0530

    Prime number

---
+++ src/com/java/primenumber.java
@@ -0,0 +1,47 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class primenumber {
+
+		public static int primeNumberCheck(int num) {
+			int i = 0;
+			int flag = 0;
+			int prime = 0;
+			for (i = 2; i <= (num / 2); i++) {
+				if (num % i == 0) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0 && num != 1)
+				prime = num;
+			return prime;
+
+		}
+
+		public static void main(String[] args) {
+
+//			int num = 0;
+			int prime = 0;
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.println("Enter the number");
+//			int num = scanner.nextInt();
+
+			for (int num = 1; num <= 10; num++) {
+				prime = primeNumberCheck(num);
+				if (prime == num) {
+					System.out.println(num);
+				}
+			}
+
+//			sum = primeNumberCheck(num);
+//			if (sum == num) {
+//				System.out.println(num + " is a prime number");
+//			} else {
+//				System.out.println(num + " is not a prime number");
+//			}
+//			scanner.close();
+
+		}
+	}
